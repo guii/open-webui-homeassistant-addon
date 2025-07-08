592eeeee5be6341ba467e2c64ba1a656a9984b8c
guii <>
2025-07-08 21:28:55 +0200

    add ingress

---
+++ open-webui/run.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/with-contenv bashio
+
+# Get configuration from Home Assistant
+CONNECTION_MODE=$(bashio::config 'connection_mode')
+PGID=$(bashio::config 'PGID')
+PUID=$(bashio::config 'PUID')
+TZ=$(bashio::config 'TZ' 'UTC')
+
+# Set environment variables
+export PGID=${PGID}
+export PUID=${PUID}
+export TZ=${TZ}
+
+bashio::log.info "Starting Open WebUI with connection mode: ${CONNECTION_MODE}"
+
+# Configure based on connection mode
+case "${CONNECTION_MODE}" in
+    "ingress_noauth")
+        bashio::log.info "Configuring for Home Assistant ingress (no auth)"
+        # Set environment variables for ingress mode
+        export WEBUI_AUTH=False
+        export WEBUI_URL_PREFIX="/api/hassio_ingress/$(bashio::addon.slug)"
+        export WEBUI_HOST="0.0.0.0"
+        export WEBUI_PORT="8080"
+        ;;
+    "ingress_auth")
+        bashio::log.info "Configuring for Home Assistant ingress (with auth)"
+        export WEBUI_AUTH=True
+        export WEBUI_URL_PREFIX="/api/hassio_ingress/$(bashio::addon.slug)"
+        export WEBUI_HOST="0.0.0.0"
+        export WEBUI_PORT="8080"
+        ;;
+    "noingress_auth")
+        bashio::log.info "Configuring for direct access (with auth)"
+        export WEBUI_AUTH=True
+        export WEBUI_HOST="0.0.0.0"
+        export WEBUI_PORT="8080"
+        ;;
+    *)
+        bashio::log.warning "Unknown connection mode: ${CONNECTION_MODE}, using default"
+        export WEBUI_HOST="0.0.0.0"
+        export WEBUI_PORT="8080"
+        ;;
+esac
+
+# Start Open WebUI
+bashio::log.info "Starting Open WebUI..."
+exec python -m open_webui.main
